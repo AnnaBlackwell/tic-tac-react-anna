c046c64a008e819e9f428e8bc5432570c8542382
Anna Blackwell <annablackwell@Annas-MacBook-Air.local>
2016-03-13 19:37:51 +1300

    working tic-tac-toe

---
+++ app/components/app.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Game from './game.jsx!'
+
+class App extends Component {
+
+  render () {
+    return <div>
+    <Game />
+    </div>
+  }
+}
+
+export default App
---
+++ app/components/square.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Square extends Component {
+  handleClick (event) {
+      if (this.props.clickCb) {
+        this.props.clickCb()
+      }
+    }
+
+    render () {
+     const winner = this.props.win
+     const player = this.props.player
+
+     const status = winner ? `${player} win` : player
+
+     return player ?
+       <div className={status}>{player}</div> :
+       <div onClick={this.handleClick.bind(this)}/>
+   }
+}
+
+export default Square
